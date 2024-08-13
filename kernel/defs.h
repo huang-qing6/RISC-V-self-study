7dab55f396fe2aac9e472a6772dcfc6b23b2f620
huang-qing6 <931362071@qq.com>
2024-08-13 09:31:53 +0800

    fix bug

--- kernel/defs.h
+++ kernel/defs.h
@@ -160,8 +160,8 @@ int             uartgetc(void);
 // vm.c
 void            kvminit(void);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
-void            kvmmap(uint64, uint64, uint64, int);
+uint64          kvmpa(pagetable_t, uint64);
+void            kvmmap(pagetable_t, uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
 void            uvminit(pagetable_t, uchar *, uint);
@@ -178,6 +178,13 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+int             vmprint(pagetable_t);
+void            kvm_free_kernelpgtbl(pagetable_t);
+pagetable_t     kvminit_newpgtbl(void);
+uint64          kvmdealloc(pagetable_t, uint64, uint64);
+int             kvmcopymappings(pagetable_t, pagetable_t, uint64, uint64);
+
+
 
 // plic.c
 void            plicinit(void);
@@ -193,7 +200,9 @@ void            virtio_disk_intr(void);
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
 
-
+// strcopy.c
+int copyin_new(pagetable_t, char *, uint64, uint64);
+int copyinstr_new(pagetable_t, char *, uint64, uint64);
 
 // stats.c
 void            statsinit(void);
